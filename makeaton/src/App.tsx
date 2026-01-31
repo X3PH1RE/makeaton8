252f66b81a980005c6e1e084296104c703ed77b2
Ashwin Menon <117213148+X3PH1RE@users.noreply.github.com>
2026-01-31 18:37:04 +0530

    added registration deadline, removed track prize text

--- makeaton/src/App.tsx
+++ makeaton/src/App.tsx
@@ -326,6 +326,33 @@ function Home() {
       >
         Register Now
       </a>
+      <div 
+        className="registration-deadline-wrapper"
+        onMouseMove={(e) => {
+          const rect = e.currentTarget.getBoundingClientRect();
+          const x = e.clientX - rect.left;
+          const y = e.clientY - rect.top;
+          const glowText = e.currentTarget.querySelector('.registration-deadline-glow-text') as HTMLElement;
+          if (glowText) {
+            glowText.style.setProperty('--mouse-x', `${x}px`);
+            glowText.style.setProperty('--mouse-y', `${y}px`);
+          }
+        }}
+        onMouseLeave={(e) => {
+          const glowText = e.currentTarget.querySelector('.registration-deadline-glow-text') as HTMLElement;
+          if (glowText) {
+            glowText.style.setProperty('--mouse-x', `-200px`);
+            glowText.style.setProperty('--mouse-y', `-200px`);
+          }
+        }}
+      >
+        <p className="registration-deadline">
+          Registrations end on 2nd Feb!
+        </p>
+        <span className="registration-deadline-glow-text">
+          Registrations end on 2nd Feb!
+        </span>
+      </div>
       <p className="festival-quote">
         If a hackathon<span className="hackathon-text"></span> could be a <span className="festival-text">Festival</span>,<br />
         it'd be <span className="make-a-ton-animated">Make-A-Ton</span>.
@@ -345,9 +372,6 @@ function Home() {
         </p>
       </div>
 
-      {/* Infinite scrolling banner below about section */}
-      <div className="scroll-banner" role="img" aria-label="Tracks and Prizes coming soon"></div>
-      
       {/* Intersecting sponsor banners with different rotations */}
       <div className="sponsor-banner sponsor-banner-1" role="img" aria-label="Wanna Sponsor US?"></div>
       <div className="sponsor-banner sponsor-banner-2" role="img" aria-label="Wanna Sponsor US?"></div>
